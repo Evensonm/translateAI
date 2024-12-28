a5f1c9b2b5e62e493df01b96d35d78e4eefd940e
Evensonm <evensonm1@gmail.com>
2024-12-28 12:36:54 -0500

    Second Commit
    Starting Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='flex flex-col p-4'>
+      <section className='min-h-screen flex flex-col'>
+        <header className='flex items-center justify-between gap-4'>
+          <h1>Ulti<span className='text-blue-400'>Translate</span></h1>
+          <button className='flex items-center gap-2'>
+            <p>New</p>
+            <i className="fa-regular fa-square-plus"></i>
+            </button>
+        </header>
+        <main className='flex-1'>
+
+        </main>
+      </section>
      <h1 className='text-green-400'>Hello</h1>
+    <footer></footer>
     </div>
   )
 }
